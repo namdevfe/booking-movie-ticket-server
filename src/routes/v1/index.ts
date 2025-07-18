ad047be83138a556e4926f589f8bd593a1003dd0
namnkq <namnkq.xherozone@gmail.com>
2025-07-18 15:54:14 +0700

    feat: add register feature

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,8 +1,10 @@
 import express from 'express'
+import { authRoutes } from '~/routes/v1/authRoute'
 import { userRoutes } from '~/routes/v1/userRoute'
 
 const Router = express.Router()
 
 Router.use('/users', userRoutes)
+Router.use('/auth', authRoutes)
 
 export const APIs_V1 = Router
